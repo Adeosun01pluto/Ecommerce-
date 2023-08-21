331e30f888663a4d6ef5573c170fa2e12036b94f
Adeosun01pluto <adeosun01pluto@gmail.com>
2023-08-21 06:06:36 -0700

    first commit

---
+++ src/commom/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+import Head from "./Head"
+import Search from "./Search"
+import Navbar from "./Navbar"
+
+const Header = ({ CartItem }) => {
+  return (
+    <>
+      <Head />
+      <Search CartItem={CartItem} />
+      <Navbar />
+    </>
+  )
+}
+
+export default Header
